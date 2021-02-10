4d8ad2b94dabf69d14e2706732fe0904bd2de819
Fabio Monteiro <fabiojr687@gmail.com>
2021-02-10 18:30:55 -0300

    testing home image

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sections.css';
 import HeaderList from './HeaderList';
+import homePic from '../images/home.jpg';
 
 const Section = () => (
     // section 1 start
     <section className="sections">
         <HeaderList />
+        <img src={homePic} alt="some text" width="1524" height="885px" />
     </section>//section 1 end 
 );
 
