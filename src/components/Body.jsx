25b89782d3d78f4428a5928d789fe6b53549d2d0
Fabio Monteiro <fabiojr687@gmail.com>
2021-02-13 20:30:27 -0300

    finished the look of session 3, general css reconfigured between 1280p and 1600p

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,10 +9,10 @@ const Body = () => (
 
     <div className="body-container">
         {/* section 1 start */}
-        <section className="sections-1">
-            <div className="sections-1-texts">
+        <section className="section-1">
+            <div className="section-1-texts">
                 <h2 className="section-1-title">The perfect way to get best property</h2>
-                <p className="section-1-subtitle">Since our start in 2014, we are profesional and many experience in real state.
+                <p className="section-1-explanation">Since our start in 2014, we are profesional and many experience in real state.
                 We will always brings you the best value out of our best property. Fato brings
                 together property buyers, tenants and investors with the best sellers, tenants,
                 real estate agents and property developers in the country.
@@ -22,7 +22,7 @@ const Body = () => (
             </div>
 
             <div className="section-1-image-container">
-                <img className="section-1-image" src={homePic} alt="house"/>
+                <img className="section-1-image" src={homePic} alt="house" />
                 <Link className="section-1-image-play" to="/">
                     <span className="section-1-play-button"
                         role="button">|&gt;</span>
@@ -31,7 +31,9 @@ const Body = () => (
             </div>
         </section>        {/* section 1 end */}
         <section className="section-2"> {/* section 2 start */}
-            <h2 className="section-2-title">Here is a little about what we do</h2>
+            <div className="section-2-title-container">
+                <h2 className="section-2-title">Here is a little about what we do</h2>
+            </div>
             <SimpleCard classes="section-2-options-container" icon={homeIcon} iconAlt="Buy a home"
                 title="Buy a home">Find your place with immersive photo experience and the most
             listings, including things you won’t find anywhere else</SimpleCard>
@@ -43,8 +45,17 @@ const Body = () => (
                 shopping on the largest rental network, to applying, to paying rent</SimpleCard>
         </section>{/* section 2 end */}
         <section className="section-3">{/* section 3 start */}
-            <div className="section-3-image-container">
-                <img src={homePic} alt="house" height="774" />
+            <div className="section-3-image-ico-container">
+                <div className="section-3-image-container">
+                    <img src={homePic} alt="house" height="774" />
+                </div>
+
+                <div className="section-3-icon-1-container">
+                    <img className="section-3-icon" src={homeIcon} alt="Negociação" />
+                </div>
+                <div className="section-3-icon-2-container">
+                    <img className="section-3-icon" src={homeIcon} alt="Negociação" />
+                </div>
             </div>
             <div className="section-3-texts-container">
                 <h2>Best way to get your dream home</h2>
@@ -66,26 +77,26 @@ const Body = () => (
 
                 <div className="section-3-options">
                     <div className="section-3-number-container">
-                        <p className="section-3-number">01</p>
+                        <p className="section-3-number">02</p>
                     </div>
                     <div className="section-3-explanation">
-                        <h3>Find a home you want</h3>
-                        <p>You need to know about a house. You can make a list of 10 of your favorite
-                        houses to make sure you still have a choice if a competitor gets picked up
-                        by another buyer.
+                        <h3>Sell your home</h3>
+                        <p>Selling your house and moving is a big transition to make, but it doesn’t
+                        need to be a stressful experience. Learn how to sell a house the right way
+                        with these seven steps!
                         </p>
                     </div>
                 </div>
 
                 <div className="section-3-options">
                     <div className="section-3-number-container">
-                        <p className="section-3-number">01</p>
+                        <p className="section-3-number">03</p>
                     </div>
                     <div className="section-3-explanation">
-                        <h3>Find a home you want</h3>
-                        <p>You need to know about a house. You can make a list of 10 of your favorite
-                        houses to make sure you still have a choice if a competitor gets picked up
-                        by another buyer.
+                        <h3>Rent a base home for you</h3>
+                        <p>If you own a house that you can't sell and are thinking of renting it
+                        out, finding good tenants who will take care of your home is
+                        challenging, but essential.
                         </p>
                     </div>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 import Body from '../components/Body';
 
 const Section = () => (
-    <div>
+    <div className="home-container">
         <Header />
         <Body />
     </div>
